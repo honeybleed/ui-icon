35d6646e3580ac12aa07b6ce67b2412ed7a772fc
Aaron <honeybleed@gmail.com>
2017-10-31 15:45:30 +0800

    init icon module project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aui-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'aui';
+}
